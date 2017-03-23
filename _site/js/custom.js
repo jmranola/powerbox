f4433f27359a90e17f35a01ae94fbd6700b366fc
unknown <jonar@ph-jonar-m.ph.trendnet.org>
2017-03-23 08:22:23 +0800

    Added About Us, Integrations and Features

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -22,3 +22,59 @@
 $(window).on( "load", function(){
      $(".loader").fadeOut("slow");
  });
+
+  $(document).ready(function() {
+    $('#img-1').fadeIn();
+
+    if (window.addEventListener) {
+      window.addEventListener('scroll', checkScrolled);
+    }
+    else {
+      window.attachEvent('scroll', checkScrolled);
+    }
+    
+    function checkScrolled() {
+            var ypos = window.pageYOffset;
+            var $heightBanner = $('.uk-section').height();
+            var $navBar = $('.navbar-header').height();
+            if ((ypos >= 0) && (ypos < ($heightBanner))) { 
+                $('#img-1').fadeIn();
+            }
+            else{
+                $('#img-1').fadeOut();
+            }
+            if ((ypos > ($heightBanner)) && (ypos < ($heightBanner*2))) { 
+                $('#img-2').fadeIn();
+            }
+            else{
+                $('#img-2').fadeOut();
+            }
+            if ((ypos > ($heightBanner*2)) && (ypos < ($heightBanner*3))) { 
+                $('#img-3').fadeIn();
+            }
+            else{
+                $('#img-3').fadeOut();
+            }
+            if ((ypos > ($heightBanner*3)) && (ypos < ($heightBanner*4))) { 
+                $('#img-4').fadeIn();
+            }
+            else{
+                $('#img-4').fadeOut();
+            }
+            if ((ypos > ($heightBanner*4)) && (ypos < ($heightBanner*5))) { 
+                $('#img-5').fadeIn();
+            }
+            else{
+                $('#img-5').fadeOut();
+            }
+            if ((ypos > ($heightBanner*5)) && (ypos < ($heightBanner*6))) { 
+                $('#img-6').fadeIn();
+            }
+            else{
+                $('#img-6').fadeOut();
+            }
+
+    }
+
+});
+ 
